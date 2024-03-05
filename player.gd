154cae9c991d290d76555131bea84846c71ca307
JElwellMSU <jelwell@murraystate.edu>
2024-03-05 17:05:23 -0500

    Added Player Node
    Added Player Node
    - Can move in different directions
    - Added sprite file to repo

---
+++ player.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@onready var screensize = get_viewport_rect().size
+@export var speed = 150
+
+func _process(delta):
+	var input = Input.get_vector("left", "right", "up", "down")
+	position += input * speed * delta
+	position = position.clamp(Vector2.ZERO, screensize)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
