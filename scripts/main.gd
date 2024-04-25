f2a187b98d4006b4752d7339ceeee84696cb599f
JElwellMSU <jelwell@murraystate.edu>
2024-04-25 00:35:36 -0400

    Fixed main scene script and added long range enemy
    Relinked the main scene script and tweaked the singleton for the level. Added enemy_long_range.
    Need to fix death animation and shooting

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
 #Loads enemies to use
-var enemy = preload("uid://dca31lgf1n6va")
-var enemyMid = preload("uid://qa58ublw1g87")
-var current_level = null
+var enemy = preload("res://scenes/enemy_rusher.tscn")
+var enemyMid = preload("res://scenes/enemy_mid_range.tscn")
+var current_level = 1
 var score = 0
-
-func _process(delta):
-	pass
 
 func _ready():
 	start()
@@ -21,6 +18,7 @@ func spawning_enemies():
 			add_child(e)
 			e.start(pos)
 			e.died.connect(_on_enemy_died)
+			print("spawning")
 	for x in range(3):
 		for y in range(1):
 			var em = enemyMid.instantiate()
@@ -28,11 +26,12 @@ func spawning_enemies():
 			add_child(em)
 			em.start(pos)
 			em.died.connect(_on_enemy_died)
+			print("spawning")
 
 #sets the current level and increments for the next level
 func start():
-	current_level == $levelInfo.get_level()
-	$levelInfo.next_level()
+	current_level = levelInfo.level
+	spawn_enemies()
 
 func spawn_enemies():
 	for x in range(pow(4, current_level)):
@@ -52,3 +51,6 @@ func spawn_enemies():
 
 func _on_enemy_died(value):
 	score += value
+
+func _process(delta):
+	pass
